1eec911d94b13e4728e327f1c9beb860f91383e2
Alice Davis <alice@gigantic.computer>
2021-07-18 00:15:56 -0700

    move gitsigns maps to g prefix

--- init.lua
+++ init.lua
@@ -227,11 +227,35 @@ vim.g.indent_blankline_show_trailing_blankline_indent = false
 -- Gitsigns
 require('gitsigns').setup {
     signs = {
-        add = { hl = 'GitGutterAdd', text = '+' },
-        change = { hl = 'GitGutterChange', text = '~' },
-        delete = { hl = 'GitGutterDelete', text = '_' },
-        topdelete = { hl = 'GitGutterDelete', text = '‾' },
+        add          = { hl = 'GitGutterAdd', text = '+' },
+        change       = { hl = 'GitGutterChange', text = '~' },
+        delete       = { hl = 'GitGutterDelete', text = '_' },
+        topdelete    = { hl = 'GitGutterDelete', text = '‾' },
         changedelete = { hl = 'GitGutterChange', text = '~' },
+    },
+    keymaps = {
+        -- Default keymap options
+        noremap = true,
+        buffer = true,
+
+        ['n ]g'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'"},
+        ['n [g'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'"},
+
+        ['n <leader>gn'] = '<cmd>lua require"gitsigns.actions".next_hunk()<CR>',
+        ['n <leader>gp'] = '<cmd>lua require"gitsigns.actions".prev_hunk()<CR>',
+
+        ['n <leader>gs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+        ['v <leader>gs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+        ['n <leader>gu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+        ['n <leader>gr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+        ['v <leader>gr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+        ['n <leader>gR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
+        ['n <leader>gp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+        ['n <leader>gb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
+
+        -- Text objects
+        ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
+        ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>'
     },
 }
 
