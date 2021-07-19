763b206347daccda40d4b0db304cbebb8ce49e3e
Alice Davis <alice@gigantic.computer>
2021-07-19 14:19:54 -0700

    properly configure Luapad

--- init.lua
+++ init.lua
@@ -446,6 +446,21 @@ endfunction
 command! Zen :call ZenMode()
 ]]
 
+-- luapad
+require 'luapad'.config{
+  count_limit = 150000,
+  error_indicator = false,
+  eval_on_move = true,
+  error_highlight = 'WarningMsg',
+  on_init = function()
+    print 'Hello from Luapad!'
+  end,
+  context = {
+    the_answer = 42,
+    shout = function(str) return(string.upper(str) .. '!') end
+  }
+}
+
 --mapped keybinding sequence timeout
 vim.o.timeoutlen = 750
 
