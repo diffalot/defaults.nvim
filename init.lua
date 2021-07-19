86d96018bbfa027ed523257e65c4a6addc61226f
Alice Davis <alice@gigantic.computer>
2021-07-19 07:10:14 -0700

    don't open trouble automatically when diagnostics are present

--- init.lua
+++ init.lua
@@ -170,8 +170,8 @@ vim.cmd [[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_li
 require("trouble").setup({
     position = "bottom", -- position of the list can be: bottom, top, left, right
     height = 15, -- height of the trouble list when position is top or bottom
-    auto_open = true, -- automatically open the list when you have diagnostics
-    auto_close = true, -- automatically close the list when you have no diagnostics
+    auto_open = false, -- automatically open the list when you have diagnostics
+    auto_close = false, -- automatically close the list when you have no diagnostics
     auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
     auto_fold = false, -- automatically fold a file trouble list at creation
 })
