8abbf05b5bf158caf7bc123ab4f708e96a22b71d
Alice Davis <alice@gigantic.computer>
2021-07-19 14:16:01 -0700

    setup lps-installer and servers, icons

--- init.lua
+++ init.lua
@@ -87,7 +87,9 @@ require('packer').startup(function()
     -- Additional textobjects for treesitter
     use 'nvim-treesitter/nvim-treesitter-textobjects'
     use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
-    use 'williamboman/nvim-lsp-installer' -- add LspInstall <server> command
+    use 'kabouzeid/nvim-lspinstall' -- add LspInstall <server> command
+    use 'onsails/lspkind-nvim' -- provides default icons for types of completion
+    use 'yamatsum/nvim-nonicons' -- more icons in a font
     use 'kosayoda/nvim-lightbulb' -- display lightbulb in gutter when lsp code actions are available
     use 'nvim-lua/lsp-status.nvim' -- statusline info for lsp client
     use 'hrsh7th/nvim-compe' -- Autocompletion plugin
@@ -660,44 +662,196 @@ vim.api.nvim_exec(
 -- Y yank until the end of line
 vim.api.nvim_set_keymap('n', 'Y', 'y$', { noremap = true })
 
--- LSP settings
-local on_attach = function(_, bufnr)
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+--  ██        ████████ ███████     ████████           ██     ██   ██
+-- ░██       ██░░░░░░ ░██░░░░██   ██░░░░░░           ░██    ░██  ░░            █████
+-- ░██      ░██       ░██   ░██  ░██         █████  ██████ ██████ ██ ███████  ██░░░██  ██████
+-- ░██      ░█████████░███████   ░█████████ ██░░░██░░░██░ ░░░██░ ░██░░██░░░██░██  ░██ ██░░░░
+-- ░██      ░░░░░░░░██░██░░░░    ░░░░░░░░██░███████  ░██    ░██  ░██ ░██  ░██░░██████░░█████
+-- ░██             ░██░██               ░██░██░░░░   ░██    ░██  ░██ ░██  ░██ ░░░░░██ ░░░░░██
+-- ░████████ ████████ ░██         ████████ ░░██████  ░░██   ░░██ ░██ ███  ░██  █████  ██████
+-- ░░░░░░░░ ░░░░░░░░  ░░         ░░░░░░░░   ░░░░░░    ░░     ░░  ░░ ░░░   ░░  ░░░░░  ░░░░░░
+--
+-- # LSP settings
+
+-- vim.lsp.set_log_level("debug")
+--
+-- needs https://github.com/microsoft/vscode-codicons/blob/master/dist/codicon.ttf
+-- setup in the terminal (kitty.conf) with ``
+require("vim.lsp.protocol").CompletionItemKind = {
+  "  Text",          -- = 1
+  "  Function",      -- = 2;
+  "  Method",        -- = 3;
+  "  Constructor",   -- = 4;
+  "  Field",         -- = 5;
+  "  Variable",      -- = 6;
+  "  Class",         -- = 7;
+  "  Interface",     -- = 8;
+  "  Module",        -- = 9;
+  "  Property",      -- = 10;
+  "  Unit",          -- = 11;
+  "  Value",         -- = 12;
+  "  Enum",          -- = 13;
+  "  Keyword",       -- = 14;
+  "  Snippet",       -- = 15;
+  "  Color",         -- = 16;
+  "  File",          -- = 17;
+  "  Reference",     -- = 18;
+  "  Folder",        -- = 19;
+  "  EnumMember",    -- = 20;
+  "  Constant",      -- = 21;
+  "  Struct",        -- = 22;
+  "  Event",         -- = 23;
+  "  Operator",      -- = 24;
+  "  TypeParameter", -- = 25;
+}
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] =
+  vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = { prefix = " " } })
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single" })
+
+local icons = require("nvim-nonicons")
+vim.fn.sign_define("LspDiagnosticsSignError",
+                   { text = icons.get("x-circle"), texthl = "LspDiagnosticsSignError" })
+
+vim.fn.sign_define("LspDiagnosticsSignWarning",
+                   { text = icons.get("alert"), texthl = "LspDiagnosticsSignWarning" })
+
+vim.fn.sign_define("LspDiagnosticsSignInformation",
+                   { text = icons.get("info"), texthl = "LspDiagnosticsSignInformation" })
+
+vim.fn.sign_define("LspDiagnosticsSignHint",
+                   { text = icons.get("comment"), texthl = "LspDiagnosticsSignHint" })
+
+-- require("vim.lsp.codelens").setup()
+
+local on_attach = function(client, bufnr)
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
     local opts = { noremap = true, silent = true }
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    -- vim.api.nvim_buf_set_keymap(bufnr, 'v', '<leader>ca', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>so', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
+    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    -- buf_set_keymap('v', '<leader>ca', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
+    buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    buf_set_keymap('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap('n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+    buf_set_keymap('n', '<leader>so', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
     vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+
+    -- vim already has builtin docs
+    if vim.bo.ft ~= "vim" then buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts) end
+
+    -- Set autocommands conditional on server_capabilities
+    if client.resolved_capabilities.document_highlight then
+        vim.api.nvim_exec([[
+            augroup lsp_document_highlight
+                autocmd! * <buffer>
+                autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+                autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+            augroup END
+        ]], false)
+    end
+
+    -- if client.resolved_capabilities.code_lens then
+    --     vim.cmd [[
+    --         augroup lsp_codelens
+    --             autocmd! * <buffer>
+    --             autocmd CursorHold,CursorHoldI,InsertLeave <buffer> lua require"lsp-codelens".buf_codelens_refresh()
+    --         augroup END
+    --     ]]
+    -- end
+
+    if client.server_capabilities.colorProvider then
+        require"lsp-documentcolors".buf_attach(bufnr, { single_column = true })
+    end
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
+-- Configure lua language server for neovim development
+local lua_settings = {
+    Lua = {
+        runtime = {
+            -- LuaJIT in the case of Neovim
+            version = "LuaJIT",
+            path = vim.split(package.path, ";"),
+        },
+        diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = { "vim" },
+        },
+        workspace = {
+            -- Make the server aware of Neovim runtime files
+            library = {
+                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+            },
+        },
+    },
+}
 
--- Enable lsp-installer installed servers
-local lsp_installed_servers = require('nvim-lsp-installer').get_installed_servers()
-for _, lsp in ipairs(lsp_installed_servers) do
-    opts = {
-        on_attach = on_attach,
+-- config that activates keymaps and enables snippet support
+local function make_config()
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+    capabilities.textDocument.colorProvider = { dynamicRegistration = false }
+    return {
+        -- enable snippet support
         capabilities = capabilities,
+        -- map buffer local keybindings when the language server attaches
+        on_attach = on_attach,
     }
-    lsp:setup(opts)
 end
+
+-- lua css html typescript graphql bash vim json yaml
+
+local function setup_servers()
+    require'lspinstall'.setup()
+    local servers = require'lspinstall'.installed_servers()
+    -- ... and add manually installed servers
+    -- table.insert(servers, "sourcekit")
+
+    for _, server in pairs(servers) do
+        local config = make_config()
+
+        -- language specific config
+        if server == "lua" then
+            config.settings = lua_settings
+            config.root_dir = function(fname)
+                if fname:match("lush_theme") ~= nil then return nil end
+                local util = require "lspconfig/util"
+                return util.find_git_ancestor(fname) or util.path.dirname(fname)
+            end
+        end
+        if server == "vim" then config.init_options = { isNeovim = true } end
+
+        -- apply the config
+        require"lspconfig"[server].setup(config)
+    end
+end
+
+setup_servers()
+
+-- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
+require'lspinstall'.post_install_hook = function ()
+  setup_servers() -- reload installed servers
+  vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+end
+
+-- UI just like `:LspInfo` to show which capabilities each attached server has
+vim.api.nvim_command("command! LspCapabilities lua require'lsp-capabilities'()")
 
 -- Treesitter configuration
 -- Parsers must be installed manually via :TSInstall
