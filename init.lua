1f8742ae5b7145119fd554f7c4a139524049ab36
Alice Davis <alice@gigantic.computer>
2021-07-18 17:14:18 -0700

    add git-messenger

--- init.lua
+++ init.lua
@@ -65,6 +65,7 @@ vim.api.nvim_exec(
 local use = require('packer').use
 require('packer').startup(function()
     use 'wbthomason/packer.nvim' -- Package manager
+    use 'rhysd/git-messenger.vim' -- display git commit message for current line
     use 'tpope/vim-fugitive' -- Git commands in nvim
     use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github
     use 'tpope/vim-commentary' -- "gc" to comment visual regions/lines
