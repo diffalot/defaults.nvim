efda5d8c61177e934ad51077da8a721899d689d7
Alice Davis <alice@gigantic.computer>
2021-07-23 04:18:59 -0700

    which-key expand column width

--- init.lua
+++ init.lua
@@ -220,7 +220,7 @@ require("which-key").setup {
     },
     layout = {
         height = { min = 4, max = 42 }, -- min and max height of the columns
-        width = { min = 20, max = 50 }, -- min and max width of the columns
+        width = { min = 20, max = 75 }, -- min and max width of the columns
         spacing = 2, -- spacing between columns
         align = "center", -- align columns left, center or right
     },
