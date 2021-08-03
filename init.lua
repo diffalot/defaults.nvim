e2a681c5fdfd5dc51db12cfcd1cc26a3022fe7b8
Alice Davis <alice@gigantic.computer>
2021-08-02 21:28:30 -0700

    add stylua from luarocks

--- init.lua
+++ init.lua
@@ -210,7 +210,7 @@ require("packer").startup(function()
     use "jeffkreeftmeijer/vim-dim"
 
     -- LuaRocks
-    use_rocks { "lua-cjson", "lua-resty-http" }
+    use_rocks { "stylua", "lua-cjson", "lua-resty-http" }
     use_rocks { "luaformatter", server = "https://luarocks.org/dev" }
 
 end)
